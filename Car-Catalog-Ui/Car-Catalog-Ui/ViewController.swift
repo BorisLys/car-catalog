18605b06e9efbf016fbbbb947cf2bec23eff06a8
boris.lysikov <festival311@gmail.com>
2020-04-09 20:32:14 +0700

    Создал проект

---
+++ Car-Catalog-Ui/Car-Catalog-Ui/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Car-Catalog-Ui
+//
+//  Created by Борис Лысиков on 09.04.2020.
+//  Copyright © 2020 Борис Лысиков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
